df633c3a9df9e05f418d5e1e28982899c4619bbc
RitaDyvnych <m.dyvnych@gmail.com>
2021-11-19 11:09:17 +0200

    набросан fetch с mockapi, данные приходят в нужном формате

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,17 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import { reducer } from './reducers';
 
+const getContact = () => {
+  const url = "https://6197549daf46280017e7e535.mockapi.io/contacts";
+  return fetch(url).then((r) => r.json()).then((data) => console.log("data", data));
+};
+
 const initData = () => {
-    const contact = localStorage.getItem("contacts");
-    if (contact) {
+  const contact = localStorage.getItem("contacts");
+
+  if (contact) {
+    // return getContact();
+    // console.log('parse', JSON.parse(contact));
       return JSON.parse(contact);
     }
     else { return [];}
